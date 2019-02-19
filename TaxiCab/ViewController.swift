56a73eec1c43dcd85ea9b737a2b34c203e0f143d
Nitish <nitish@maropost.com>
2019-02-19 16:46:12 +0530

    Initial Commit

---
+++ TaxiCab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaxiCab
+//
+//  Created by maropost on 19/02/19.
+//  Copyright © 2019 maropost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
